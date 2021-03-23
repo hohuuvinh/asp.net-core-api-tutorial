3b09779458134605f917b249eb663c92bc9d82d4
Vinh Ho <vinhhofb@gmail.com>
2021-03-23 20:02:59 +0700

    f

---
+++ CRUD-api/Web.DemoAPI/Web.DemoAPI/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web.DemoAPI.Models;
+
+namespace Web.DemoAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Route("api/GetData")]
+        [HttpGet]
+        public string GetData()
+        {
+            return "dhđgfgfg";
+        }
+
+        [Route("api/GetDataDetail/{name}")]
+        [HttpGet]
+        public string GetDataDetail(string name)
+        {
+            return name;
+        }
+        [Route("api/PostData/{name}")]
+        [HttpPost]
+        public Data PostData([FromUri]string name,  Data data)
+        {
+            return data;
+        }
+    }
+    
+}
---
+++ CRUD-api/Web.DemoAPI/Web.DemoAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web.DemoAPI.WebApiApplication" Language="C#" %>
